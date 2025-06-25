ec42c178777cd1202cd7b0a3ef75537f78275597
maymashire <HP@DESKTOP-N1HAESP>
2025-06-25 15:28:43 -0700

    ttt

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("wax tusid");
+using System.Threading.Tasks.Dataflow;
+
+//Console.WriteLine("wax tusid");
+
+string stdname = "maryan mohamutt shire";
+string m_name = "suweys ahmett mohamutt";
+string dop= "2/2/200";
+string pob= "galkio mudug";
+string email= "maryanmohamutt54@gmail.com";
+string sex= "female";
+string status= "single";
+string facult= "computer science";
+int telephone= 0907919229;
+
+
+char selection;
+double pi = 13.4;
+
+
+Console.WriteLine("magca ardayga waa " +stdname);
+Console.WriteLine("magca ardayga hooyo waa " + m_name);
+Console.WriteLine("dhalashadeydu waa " +dop );
+Console.WriteLine("meesha kudhashey " + pob);
+Console.WriteLine("emailkygu waa " + email);
+Console.WriteLine("sex waa " + sex);
+Console.WriteLine("status waa " + status);
+Console.WriteLine("facultgygu waa " + facult);
+Console.WriteLine("tell " + telephone);
+//Console.WriteLine("itusid double" + pi);
